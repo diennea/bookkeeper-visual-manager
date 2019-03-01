0a12e05e96172b7f3424b0304bc04d6b4357cf3d
Enrico Olivelli - Diennea <enrico.olivelli@diennea.com>
2019-03-01 17:23:07 +0100

    Initial commit

---
+++ src/main/java/org/bookkepervisualmanager/api/GenericResource.java
@@ -0,0 +1,51 @@
+/*
+ Licensed to Diennea S.r.l. under one
+ or more contributor license agreements. See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership. Diennea S.r.l. licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+
+ */
+package org.bookkepervisualmanager.api;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Main API
+ */
+@Path("generic")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> get() {
+        return new HashMap<>();
+    }
+}
