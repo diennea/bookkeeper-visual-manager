bcc2d4ea22c588d69d1cccf5a951075b5831360f
Matteo Minardi <matteo.minardi@diennea.com>
2019-05-23 13:33:19 +0200

    Added vue ledgers implementation

--- src/main/bvmui/src/components/Title.vue
+++ src/main/bvmui/src/components/Title.vue
@@ -11,15 +11,7 @@ export default {
     },
     computed: {
         computedTitle() {
-            let string = this.title;
-            let placeholders = this.params;
-            for (var placeholder in placeholders) {
-                string = string.replace(
-                    new RegExp("\\${" + placeholder + "}", "g"),
-                    placeholders[placeholder]
-                );
-            }
-            return string;
+            return this.$library.replacePlaceholders(this.title, this.params);
         }
     }
 };
--- src/main/bvmui/src/lib/library.js
+++ src/main/bvmui/src/lib/library.js
@@ -9,5 +9,15 @@ export default {
         const i = Math.floor(Math.log(bytes) / Math.log(k));
 
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+    },
+    replacePlaceholders(string, placeholders) {
+        const _placeholders = !placeholders ? {} : placeholders;
+        for (var placeholder in _placeholders) {
+            string = string.replace(
+                new RegExp("\\${" + placeholder + "}", "g"),
+                placeholders[placeholder]
+            );
+        }
+        return string;
     }
 }
