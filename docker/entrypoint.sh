d47fca5d04c4a1a944519d08ab18aadb9ebfec32
Nicolò Boschi <boschi1997@gmail.com>
2022-03-10 03:04:48 +0100

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 env | while IFS='=' read -r n v; do
     if [[ "$n" == BKVM_* ]]; then
       property=${n/BKVM_/}
-      property=${property/\./_}
+      property=${property/_/\.}
       escaped_value=$(printf '%s\n' "$v" | sed -e 's/[\/&]/\\&/g')
       sed -i "s/$property.*/$property=$escaped_value/g" /bkvm/conf/server.properties
-      printf "Replacing configuration entry %s with %s\n" "$property" "$v"
+      printf "Setting configuration entry %s: %s\n" "$property" "$v"
     fi
 done
 
